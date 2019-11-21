3e5a4b708e96c06addf057d6fe146897c6bdef97
M B S Yaswanth <mbs.yaswanth@gmail.com>
2019-11-21 15:44:01 +0530

    now loading translations from public folder using xhr requests

--- src/i18n/index.tsx
+++ src/i18n/index.tsx
@@ -2,22 +2,16 @@ import i18n from 'i18next'
 import Backend from 'i18next-xhr-backend'
 import { initReactI18next } from 'react-i18next'
 
-import translationEN from './translations/en.json'
-
 const fallbackLng = ['en']
-
-const resources = {
-   en: {
-      translation: translationEN
-   }
-}
 
 i18n
    .use(Backend)
    .use(initReactI18next)
    .init({
-      resources,
-      fallbackLng
+      fallbackLng,
+      backend: {
+         loadPath: '/i18n/translations/{{lng}}.json'
+      }
    })
 
 export default i18n
