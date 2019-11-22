8e0da250992b45331bbb23580cc10f8d68d2198d
prasanth-277 <prasanthreddy549@gmail.com>
2019-11-22 14:41:30 +0530

    created image component

---
+++ src/common/components/Image/Image.stories.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import Image from '.'
+
+storiesOf('Common Component Guide', module).add('Image Component', () => (
+   <Image />
+))
