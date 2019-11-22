f00935907219b8197cfcc3216b49306a66b32aad
prasanth-277 <prasanthreddy549@gmail.com>
2019-11-22 15:48:34 +0530

    added pagination loader

---
+++ src/common/components/PaginationLoader/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component'
+
+// https://github.com/ankeetmaini/react-infinite-scroll-component
+
+function PaginationLoader(props) {
+   return <InfiniteScroll {...props} />
+}
+export default PaginationLoader
