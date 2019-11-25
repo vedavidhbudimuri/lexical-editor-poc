093692f594d09c9ceaa0b58de75de6d61665571d
vamsimudadla <mudadlavamsi@gmail.com>
2019-11-25 10:17:21 +0530

    added hi.json file and removed React.FC in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,25 @@ import { withTranslation } from 'react-i18next'
 import logo from './logo.svg'
 import './App.css'
 
-const App: React.FC<{
-   t: any
-}> = ({ t }) => {
-   return (
-      <div className='App'>
-         <header className='App-header'>
-            <img src={logo} className='App-logo' alt='logo' />
-            <p>
-               Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a
-               className='App-link'
-               href='https://reactjs.org'
-               target='_blank'
-               rel='noopener noreferrer'
-            >
-               {t('app.learn_react')}
-            </a>
-         </header>
-      </div>
-   )
-}
+type AppProps = { t: any }
+
+const App = ({ t }: AppProps) => (
+   <div className='App'>
+      <header className='App-header'>
+         <img src={logo} className='App-logo' alt='logo' />
+         <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+         </p>
+         <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+         >
+            {t('app.learn_react')}
+         </a>
+      </header>
+   </div>
+)
 
 export default withTranslation()(App)
