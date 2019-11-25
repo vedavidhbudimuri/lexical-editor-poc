809cfb5fc6e9af5ce9b12a1ef220d0661c0693ca
prasanth-277 <prasanthreddy549@gmail.com>
2019-11-25 10:55:34 +0530

    implemented HorizontalScroller

--- src/common/components/Button/Button.stories.tsx
+++ src/common/components/Button/Button.stories.tsx
@@ -5,6 +5,14 @@ import { API_FETCHING } from '@ib/api-constants'
 
 import Button from '.'
 
-storiesOf('Component Guide', module).add('Button Component', () => (
-   <Button text='click' onClick={action('clicked')} />
-))
+storiesOf('Component Guide', module)
+   .add('Button Component', () => (
+      <Button text='click' onClick={action('clicked')} />
+   ))
+   .add('Button Component with apiStatus', () => (
+      <Button
+         text='click'
+         onClick={action('clicked')}
+         apiStatus={API_FETCHING}
+      />
+   ))
---
+++ src/common/components/HorizontalPaginationScroller/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import HorizontalScrolling from 'react-horizontal-scrolling-menu'
+
+// https://github.com/asmyshlyaev177/react-horizontal-scrolling-menu
+
+function HorizontalPaginationScroller(props) {
+   return <HorizontalScrolling {...props} />
+}
+
+export default HorizontalPaginationScroller
