b4200bd099f6c57a8cb5c73dfc6aa2fcd097f5ff
Anesh Parvatha <anesh@cybereyelabs.io>
2019-11-27 15:12:14 +0530

    Add i18n provider as decorator in storybook

--- src/common/components/Button/Button.stories.tsx
+++ src/common/components/Button/Button.stories.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { API_FETCHING } from '@ib/api-constants'
+import { I18nextProvider } from 'react-i18next'
 
-import styled from 'styled-components'
+import i18n from '../../../i18n'
 
 import Button from '.'
 
 storiesOf('Component Guide', module)
+   .addDecorator(story => (
+      <I18nextProvider i18n={i18n}>{story()}</I18nextProvider>
+   ))
    .add('Button Component', () => (
       <Button text='Click' onClick={action('clicked')} />
    ))
