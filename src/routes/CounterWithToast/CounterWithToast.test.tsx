7c731df08b29d609f0df1c13fec24e7bf72da50a
Anesh Parvatha <anesh@cybereyelabs.io>
2019-11-27 11:04:32 +0530

    Install ib internal pkgs

--- src/routes/CounterWithToast/CounterWithToast.test.tsx
+++ src/routes/CounterWithToast/CounterWithToast.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { Provider } from 'mobx-react'
 
 import stores from '../../stores'
 
@@ -10,11 +9,9 @@ import CounterWithToast from './index'
 describe('Counter test', () => {
    it('should test if Counter is rendered or not', () => {
       const counter = render(
-         <Provider {...stores}>
-            <Router>
-               <CounterWithToast />
-            </Router>
-         </Provider>
+         <Router>
+            <CounterWithToast counterStore={stores.counterStore} />
+         </Router>
       )
 
       expect(counter).toBeDefined()
