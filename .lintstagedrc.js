356919981f41c7a33a4380c9da1c53a8c59b7c6a
vamsimudadla <mudadlavamsi@gmail.com>
2019-12-16 13:13:43 +0530

    added api utils

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,14 @@
 module.exports = {
-   '**/*.ts?(x)': ['eslint --fix', 'prettier --write', 'git add -A'],
-   '**/*.js?(x)': ['eslint --fix', 'prettier --write', 'git add -A'],
+   '**/*.ts?(x),!src/utils/AuthAPIUtils.ts,!src/Common/utils/APIUtils.ts': [
+      'eslint --fix',
+      'prettier --write',
+      'git add -A'
+   ],
+   '**/*.js?(x),!src/utils/AuthAPIUtils.ts,!src/Common/utils/APIUtils.ts': [
+      'eslint --fix',
+      'prettier --write',
+      'git add -A'
+   ],
    '*.json?(x)': ['prettier --write', 'git add -A'],
    '*.md?(x)': ['prettier --write', 'git add -A']
 }
