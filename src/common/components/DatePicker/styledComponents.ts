44decf161c87ba64c005693c3d634814a2631968
jagadishreddy <jagadishreddykovvuri@gmail.com>
2020-02-11 12:04:58 +0530

    check box group updated

--- src/common/components/DatePicker/styledComponents.ts
+++ src/common/components/DatePicker/styledComponents.ts
@@ -1,6 +1,16 @@
 import styled from 'styled-components'
 
+import { mobile } from '../../../utils/MixinUtils'
+
 export const DateContainer = styled('div')`
-   margin-top: 20px;
    width: 100%;
+   ${mobile} {
+      margin-top: 8px;
+   }
+`
+
+export const ErrorView = styled.div`
+   flex-direction: row;
+   margin-top: 3px;
+   margin-left: 3px;
 `
