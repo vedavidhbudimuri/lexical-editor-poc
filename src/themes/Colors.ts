d899e83cb5e0b5a35b23355c1cbf7fe0fb300eba
jagadishreddy <jagadishreddykovvuri@gmail.com>
2020-02-11 11:10:51 +0530

    ButtonWithLoader updated

--- src/themes/Colors.ts
+++ src/themes/Colors.ts
@@ -1,10 +1,11 @@
 const colors = {
+   azulTwo: '#1665d8',
    black: '#000000',
+   paleGrey: '#f6f7f9',
    primaryColor: '#2196f3',
    pinkishOrange: '#ff6b3c',
    white50: 'rgba(255,255,255,0.5)',
-   white: 'rgba(255,255,255,1)',
-   paleGrey: '#f6f7f9'
+   white: 'rgba(255,255,255,1)'
 }
 
 export default colors
